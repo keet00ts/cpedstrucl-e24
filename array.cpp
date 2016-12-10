1cf92ae3905ebd69da729335ab9727beb6a59bb9
keet00ts <keet00ts24@gmail.com>
2016-12-09 18:58:09 -0800

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main ()
+{
+	int array[5] = {1,4,3,4,4};
+	int x;
+	
+	for(x=0;x<5;x++)
+	{
+		cout << array[x] << endl;
+	}
+	
+	
+	system("pause>0");
+	
+	return 0;
+  }
